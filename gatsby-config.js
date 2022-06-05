21850e31bb565e1a75a1e216ea100ec3ec92e606
Andrew Miller <info@akmiller.co.uk>
2022-06-05 10:25:06 +0100

    update config and packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-const dotenv = require('dotenv')
+require('dotenv').config({
+  path: `.env`,
+})
 const config = require('./src/config/site')
 const prismicRepositoryName = 'thec3'
 
@@ -31,20 +33,21 @@ module.exports = {
         repositoryName: prismicRepositoryName,
         accessToken: process.env.PRISMIC_ACCESS_TOKEN,
         linkResolver: ({ node, key, value }) => linkResolver,
-        schemas: {
-          academy_page: require('./src/schemas/academy_page.json'),
-          content_page: require('./src/schemas/content_page.json'),
-          homepage: require('./src/schemas/homepage.json'),
-          landing_page: require('./src/schemas/landing_page.json'),
-          notification_banner: require('./src/schemas/notification_banner.json'),
-          redirect: require('./src/schemas/redirect.json'),
-          site_config: require('./src/schemas/site_config.json'),
-          text_page: require('./src/schemas/text_page.json'),
-          colour: require('./src/schemas/colour.json'),
-          page: require('./src/schemas/page.json'),
-          online_event: require('./src/schemas/online_event.json'),
-          contact_information: require('./src/schemas/contact_information.json'),
-        },
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        // schemas: {
+        //   academy_page: require('./src/schemas/academy_page.json'),
+        //   content_page: require('./src/schemas/content_page.json'),
+        //   homepage: require('./src/schemas/homepage.json'),
+        //   landing_page: require('./src/schemas/landing_page.json'),
+        //   notification_banner: require('./src/schemas/notification_banner.json'),
+        //   redirect: require('./src/schemas/redirect.json'),
+        //   site_config: require('./src/schemas/site_config.json'),
+        //   text_page: require('./src/schemas/text_page.json'),
+        //   colour: require('./src/schemas/colour.json'),
+        //   page: require('./src/schemas/page.json'),
+        //   online_event: require('./src/schemas/online_event.json'),
+        //   contact_information: require('./src/schemas/contact_information.json'),
+        // },
       },
     },
     `gatsby-plugin-postcss`,
